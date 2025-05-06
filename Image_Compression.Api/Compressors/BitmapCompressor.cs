0f73aed20e5890c14e855510809ba2692755bde6
SardorSohinazarov <sardorstudent0618@gmail.com>
2025-05-06 14:25:01 +0500

    Bit map

---
+++ Image_Compression.Api/Compressors/BitmapCompressor.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Image_Compression.Api.Compressors
+{
+    public class BitmapCompressor : Compressor, ICompressor
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public BitmapCompressor(IWebHostEnvironment webHostEnvironment)
+            : base(webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task CompressAsync(IFormFile file, string fileId)
+        {
+            throw new NotImplementedException("Bitmap compression is not implemented yet.");
+        }
+    }
+}
